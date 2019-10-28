194ce01b260722d507852e45fc9d62ee4c1c8c2c
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:55:42 +0100

    added meta confirm sms method

--- application/controllers/Gettew_dashboard_cont.php
+++ application/controllers/Gettew_dashboard_cont.php
@@ -225,17 +225,13 @@ $_SESSION['receivers'] = $number_arrays;
 
 
 
-
-
-    $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew | Confirm SMS";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-   
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Sign In";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+    
  $data['school'] = $this->schools_model->get_school_by_id($_SESSION['school_id']);    
           $this->load->view('users/admin/common/header_view',$data);
           $this->load->view('users/admin/common/nav_view',$data);
