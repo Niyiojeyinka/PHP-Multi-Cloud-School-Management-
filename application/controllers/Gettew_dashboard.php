4ac0cf636261c1ca476d562f2b9f4c421abbe6cc
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:25:24 +0100

    added dynamic meta detaiils to the change_password method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -97,14 +97,13 @@ $this->form_validation->set_rules("confirm_password","Confirm Password","trim|re
 
 
 if (!$this->form_validation->run()) {
-   $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew |  Admin Dashboard";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+     $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Change Password";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 
 $cond = array(
 'school_id' => $_SESSION['school_id']
