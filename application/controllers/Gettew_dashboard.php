e46d50784860cc7cd4d086b218e34e5b41ba374d
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:43:41 +0100

    added dynamic meta detaiils to the search staff  method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -1791,19 +1791,13 @@ $input_text = $this->input->post('search');
 
 
 
-
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-$data["title"] ="Gettew |  Search Staff";
-$data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-$data["author"] ="Ojeyinka olaniyi philip";
-$data["descriptions"] ="Online and offline school Management Service for schools
-and colleges";
-$data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
-
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Search Staff";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 
 
       $this->load->view('users/admin/common/header_view',$data);
