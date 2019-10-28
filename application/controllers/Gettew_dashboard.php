66842d8562ae76757f1e2c91372f575ea7427a84
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:38:17 +0100

    added dynamic meta detaiils to the staff members  method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -1127,18 +1127,13 @@ $_SESSION['action_status_report'] = "<span class='w3-text-green'>Student Optons 
 
 
 
-
-
-
-    $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew | Staff Members";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Staff Members";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 
           $this->load->view('users/admin/common/header_view',$data);
           $this->load->view('users/admin/common/nav_view',$data);
