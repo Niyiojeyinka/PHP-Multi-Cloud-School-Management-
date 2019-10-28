55b960f44f1510d102c03649e243dbd89849b19f
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:47:05 +0100

    added dynamic meta detaiils to the add offline payment  method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -2048,16 +2048,13 @@ $this->form_validation->set_rules('det','Session Option','required');
 
 if (!$this->form_validation->run()){
   
-
-    $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew |  Add Offline Payments";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Add offline Payment";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 
 $data['school'] = $this->schools_model->get_school_by_id($_SESSION['school_id']);
 $data['school_sessions'] = json_decode($data['school']['session'],true);
