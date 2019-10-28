b7df57a3075cf8a56f38e8984a92c85ed3e0a7b2
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:46:23 +0100

    added dynamic meta detaiils to the upgrde   method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -2140,16 +2140,13 @@ if (($school['license'] == "active" && time() < $school['license_expire']) || ($
 
 $this->form_validation->set_rules('domain','Domain','required');
 if (!$this->form_validation->run()) {
-    $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew | Upgrade Account";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
-
+      $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Sign In";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
           $this->load->view('users/admin/common/header_view',$data);
           $this->load->view('users/admin/common/nav_view',$data);
           $this->load->view('users/admin/common/sidebar_view',$data);
