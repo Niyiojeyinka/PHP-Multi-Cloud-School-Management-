84018a0c1be6cce0c310d481e919e8fa1a474c95
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:35:02 +0100

    added dynamic meta detaiils to the idcard  method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -886,16 +886,13 @@ public function process_nav_search()
 public function idcard($student_id)
 {
 
-
-
-    $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew |  Generate QR Code";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Generate QR Code";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 $data['student_id'] = $student_id;
 
 $this->load->view('users/admin/common/header_view',$data);
