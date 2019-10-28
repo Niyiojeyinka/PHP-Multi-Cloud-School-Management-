d5cf89755f7508490da33dc8abf5b4082d7e2f13
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 00:26:25 +0100

    added dynamic meta detaiils to the manage_subject method

--- application/controllers/Gettew_dashboard.php
+++ application/controllers/Gettew_dashboard.php
@@ -158,14 +158,13 @@ public function manage_subject()
   if(!$this->form_validation->run())
   {
 
- $data['web_favicon_slug'] = "assets/images/favicon.ico";
-    $data['description'] = NULL;
-    $data["title"] ="Gettew | Add Subject";
-    $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-    $data["author"] ="Ojeyinka olaniyi philip";
-    $data["descriptions"] ="Online and offline school Management Service for schools
-    and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+  $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." |Manage Subject";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
+ 
 
 $data['added_subjects'] = $this->schools_model->get_subjects($_SESSION['school_id']);
 
