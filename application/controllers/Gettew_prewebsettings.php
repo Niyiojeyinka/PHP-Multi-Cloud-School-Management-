15e63196678ba63ff2c82246fbfe336a492ac21d
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 01:35:20 +0100

    added meta search theme  method

--- application/controllers/Gettew_prewebsettings.php
+++ application/controllers/Gettew_prewebsettings.php
@@ -207,16 +207,12 @@ show_page("Gettew_prewebsettings_action/choose_theme/".$this->input->post('web_a
 
 
 
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-$data["title"] ="Gettew |  Search Themes";
-$data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-$data["author"] ="Ojeyinka olaniyi philip";
-$data["descriptions"] ="Online and offline school Management Service for schools
-and colleges";
-$data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
-
+    $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Search Themes";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 
           $this->load->view('users/admin/common/header_view',$data);
           $this->load->view('users/admin/common/nav_view',$data);
