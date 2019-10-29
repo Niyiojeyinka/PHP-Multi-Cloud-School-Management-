60e11aee9ed18d336fb5fb1a7e8f714057679870
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-29 01:33:19 +0100

    added meta index  method

--- application/controllers/Gettew_prewebsettings.php
+++ application/controllers/Gettew_prewebsettings.php
@@ -145,24 +145,12 @@ show_page("Gettew_prewebsettings_action/choose_theme/".$this->input->post('web_a
 
 
 
-
-
-
-
-
-
-
-
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-      $data["title"] ="Gettew |  Your School Website in few clicks";
-      $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-      $data["author"] ="Ojeyinka olaniyi philip";
-     $data["descriptions"] ="Online and offline school Management Service for schools
-     and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+    $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Your website school in few minutes";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 
 
           $this->load->view('users/admin/common/header_view',$data);
