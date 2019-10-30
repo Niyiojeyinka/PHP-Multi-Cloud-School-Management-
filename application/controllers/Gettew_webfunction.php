4b161abdd5d682c74dca0e273deadfdc330b5123
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-30 05:39:31 +0100

    added meta details to the manage slider  method

--- application/controllers/Gettew_webfunction.php
+++ application/controllers/Gettew_webfunction.php
@@ -748,20 +748,12 @@ public function manage_sliders($address_id =NULL,$offset = 0)
 
 
 
-
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-      $data["title"] ="Gettew |  Manage Sliders";
-      $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-      $data["author"] ="Ojeyinka olaniyi philip";
-     $data["descriptions"] ="Online and offline school Management Service for schools
-     and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
-
-
+     $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Manage Slider";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 $cond = array(
 "subdomain" => $address_id
 );
