0ed54a6093175f4d916160f6e7271a782ba08481
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-30 05:43:31 +0100

    added meta details to the manage school media method

--- application/controllers/Gettew_webfunction.php
+++ application/controllers/Gettew_webfunction.php
@@ -1145,20 +1145,12 @@ public function manage_media($address_id =NULL,$offset = 0)
     $data['pagination'] = $this->pagination->create_links();
 
 
-
-
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-      $data["title"] ="Gettew |  Manage Media";
-      $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-      $data["author"] ="Ojeyinka olaniyi philip";
-     $data["descriptions"] ="Online and offline school Management Service for schools
-     and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
-
+     $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Manage Media";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 
 $cond = array(
 "subdomain" => $address_id
