37af1e0a7758d4162af543bfd9559e4a32e5a725
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-10-30 05:37:19 +0100

    added meta details to the add page  method

--- application/controllers/Gettew_webfunction.php
+++ application/controllers/Gettew_webfunction.php
@@ -501,18 +501,12 @@ $data['theme_options'] = json_decode($theme['admin_options'],true);
 public function add_page($address_id =NULL)
 {
 
-
-
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-      $data["title"] ="Gettew |  Add Page";
-      $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-      $data["author"] ="Ojeyinka olaniyi philip";
-     $data["descriptions"] ="Online and offline school Management Service for schools
-     and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
+     $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Add Page";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 $data['show_editor'] = true;
 
 $cond = array(
