c8419f134b5fa8cf116285be31e5a37517be0359
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-01 23:19:44 +0100

    updated student search meta variables

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -763,23 +763,15 @@ $data['school'] = $this->schools_model->get_school_by_id($_SESSION['school_id'])
 
 
 
+ $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Search Students";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 
 
-
-
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-$data["title"] ="Gettew |  Search Students";
-$data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-$data["author"] ="Ojeyinka olaniyi philip";
-$data["descriptions"] ="Online and offline school Management Service for schools
-and colleges";
-$data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
 $data['school'] = $this->schools_model->get_school_by_id($_SESSION['school_id']);
-
-
-
-
     $this->load->view('common/head_meta_view',$data);
         $this->load->view('users/staff/common/header_view',$data);
     $this->load->view('users/admin/gettew_student_search_view',$data);
