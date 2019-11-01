9ed42db28958455bd9bc0687236feaf0c269d24b
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-01 23:15:37 +0100

    updated staff edit article method meta details

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -358,15 +358,13 @@ $this->form_validation->set_rules('contents','Article Contents','required');
 
 if (!$this->form_validation->run()) {
   
+ $data['web_favicon_slug'] = "assets/images/favicon.ico";
+      $data['title'] = $this->siteName." | Edit Article";
+      $data['author'] =  $this->author;
+      $data['keywords'] =  $this->keywords;
+      $data['description'] =  $this->description;
+      $data["noindex"] =  $this->noindex;
 
-$data['web_favicon_slug'] = "assets/images/favicon.ico";
-$data['description'] = NULL;
-      $data["title"] ="Gettew |  New Article";
-      $data["keywords"] ="gettew,school,free,Management,Software,result,checking";
-      $data["author"] ="Ojeyinka olaniyi philip";
-     $data["descriptions"] ="Online and offline school Management Service for schools
-     and colleges";
-    $data["noindex"] ='<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
 $data['school'] = $this->schools_model->get_school_by_id($_SESSION['school_id']);
 $data['item'] = $this->blog_model->get_post_by_slug($slug);
 
