b062027bb251b327edb7c7b1df13b5708075d945
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-12-23 19:48:31 +0100

    removing gettew_ from resultmanager controller and its views

--- application/controllers/Resultmanager.php
+++ application/controllers/Resultmanager.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gettew_resultmanager extends CI_Controller {
+class Resultmanager extends CI_Controller {
 
 public function __construct()
 {
