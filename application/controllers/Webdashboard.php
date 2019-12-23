051a81b70b6cdb955414f649fd36a016281b0bd4
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-12-23 19:51:05 +0100

    removing gettew_ from webfunction class name

--- application/controllers/Webdashboard.php
+++ application/controllers/Webdashboard.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gettew_webdashboard extends CI_Controller {
+class Webdashboard extends CI_Controller {
 
 public function __construct()
 {
