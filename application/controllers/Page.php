bdcbce30067a13600f0c01f2cfc1f8871da0316b
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-12-24 07:04:34 +0100

    updated meta details in page construct

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -11,14 +11,16 @@ public function __construct()
      $this->load->library(array('form_validation','session'));
 
 
+    $meta= $this->back_model->get_site_meta();
 
-   $this->siteName = $this->back_model->get_system_variable("site_name");
-      $this->author = $this->back_model->get_system_variable("author");
-      $this->keywords = $this->back_model->get_system_variable("keywords");
-      $this->description= $this->back_model->get_system_variable("description");
-      $this->tagLine= $this->back_model->get_system_variable("tagline");
+   
+     $this->tagLine =$meta['title'];
+     $this->siteName=$meta['sitename'];
+     $this->keywords=$meta['keywords'];
+     $this->description=$meta['description'];
+     $this->author=$meta['author'];
       $this->noindex = '<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
-
+     $this->favicon = base_url('assets/images/favicon.ico');
 
 }
 
